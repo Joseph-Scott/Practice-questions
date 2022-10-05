c34fe6d399f6b1f69add304d7213911a20efe3d3
Joe Scott <jbs198585@gmail.com>
2022-10-05 14:18:11 -0400

    Practice questions

---
+++ JuniorPracticeQs.js
@@ -0,0 +1,109 @@
+// add Apple at the beginning of fruits
+// remove Lemon
+// add more fruits and sort them
+var fruits = ['Pineapple', 'Lemon', 'Orange'];
+
+// use integers to their absolute values
+var integers = [1,-0,9,-8,3];
+
+// multiply an array of numbers by their position in the array
+var numbers = [1,2,3,4];
+
+// capitalize every other word in a string.
+var str = 'hello world how ya doing?';
+
+// remove all negative numbers
+var negatives = [-2,-1,0,1,2];
+
+// sum up all the values of an array
+var numbers = [1,2,3,4,5];
+
+// find the largest number
+var numbers = [1,2,3,4,5];
+
+// Please create a function using a functional programming pattern that can parse the following string. I love %s, %s, and %s because it makes me feel %s. 
+// The %s will be match (in order the following array). [ "Javascript", "AngularJS", "NodeJS", "Awesome" ]
+// The outputted string should look like this I love Javascript, AngularJS, and NodeJS because it makes me feel Awesome.
+
+let message = "I love %s, %s, and %s because it makes me feel %s".replace('%s', '');
+let args = [ "Javascript", "AngularJS", "NodeJS", "Awesome" ];
+  
+let parseString = ( string, args ) => {
+  return;
+}
+
+// solve the fibonacci sequence
+function fibonacci(num){
+	return;
+}
+
+// solve for factionals
+function factional(num){
+	return;
+}
+
+// remove duplicates of this array 
+var array = [1, 2, 3, 5, 1, 5, 9, 1, 2, 8];
+
+// Verbal Questions
+// 1. ID vs Class
+// 2. Semantic HTML?
+// 3. Is HTML/CSS/JS case sensitive?
+// 4. Sass vs CSS vs Less
+// 5. ECMAScript?
+// 6. Namespace?
+// 7. Closure?
+// 8. Language vs Framework vs Library?
+// 9. jQuery vs VanillaJS - When use either? (Hint: DOM Manipulation / Animation)
+// 10. Typical Problem Solving for Code? (1. Syntax/Logic 2. ConsoleLog/Debug 3.Google 4.Ask for Help after 1hr)
+// 11. Popular types of design patterns in JS? (Modular, Observer, Behavioral)
+// 12. OOP vs Procedural? (Know foundations of OOP)
+// 13. Join vs Split
+// 14. Pure vs Impure Functions
+// 15. Shallow vs Deep Copy
+// 16. using *this*
+// 17. Recursion?
+// 18. var vs let vs const
+// 19. Variable hosting?
+// 20. Anonymous Functions?
+// 21. Callbacks?
+// 22. For vs While Loops?
+// 23. Truthy vs Falsey?
+// 24. = vs == vs ===
+// 25. Null vs Undefined?
+// 26. JS Data Types
+// 27. NPM?
+// 28. Choosing Custom Framework vs 3rd Party Framework (Angular)?
+// 29. NodeJS?
+// 30. Single thread vs Multi thread?
+// 31. Event loop in JS?
+// 32. Promises? 
+// 33. Async / Await?
+// 34. AJAX?
+// 35. JSON? 
+// 36. Stringify vs Parse?
+// 37. Bind vs Call vs Apply?
+// 38. Reduce vs Map vs Filter?
+// 39. Association? 
+// 40. Aggregiation?
+// 41. Composition?
+// 42. RegEX?
+// 43. Encapsulation?
+// 44. Polymorphism?
+// 45. Class vs Object vs Instance?
+// 46. Push vs Pop vs Unshift vs Shift?
+// 47. Async vs Sync?
+// 48. Inline vs Block
+// 49. Static vs Relative vs Absolute vs Fixed?
+// 50. Value vs Reference
+// 51. Key Value Pairs?
+// 52. SQL vs NoSQL
+// 53. Responsiveness in CSS?
+// 54. Lexical Scope?
+// 55. getElementByID vs getElementByClassName vs getElementByTagName vs querySelector
+// 56. PX vs EM vs %
+// 57. Restful API?
+// 58. Parameter vs Argument?
+// 59. Status Codes?
+// 60. Length vs toString vs Splice vs Slice vs Reverse vs Sort vs forEach vs Concat vs indexOf
+// 61. Inheritance?
